56b0e73cb1264d0098b4e084e6185c56e738c67f
yadan <yadan@yadan.com>
2017-06-11 13:23:38 +0800

    first install       ============       update       =============       to thinkphp
          =====
          http://domian.com/mns/15889984263/xuying
          ======================1.0.5

--- src/index.php
+++ src/index.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * =============================================================
+ */
 require_once(dirname(dirname(dirname(__FILE__))).'/aliyun_mns/mns-autoloader.php');
 \think\Route::get('mns/[:moblie]/[:meg]', "\\think\\mns\\MnsController@index");
 ?>
