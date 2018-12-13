fd9ad07c2d6a4da04b30cae58f25d00015e6e8f0
yadan <15889984263@163.com>
2018-12-13 16:20:41 +0800

    thinkphp route update

--- src/index.php
+++ src/index.php
@@ -2,5 +2,9 @@
 /**
  * =============================================================
  */
-\think\Route::get('mns/[:moblie]', "\\think\\mns\\MnsController@index");
+
+//兼容新版本5.1
+Route::get('mns/[:moblie]', "\\think\\mns\\MnsController@index");
+Log::write('新版本Thinkphp5.1','notice');
+
 ?>
